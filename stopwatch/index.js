2c142968e78b66b0813000b37fb657eea157db35
Akarsh <akarshbalu1@gmail.com>
2024-07-03 22:48:03 +0530

    added play pause stop to stopwatch button

--- stopwatch/index.js
+++ stopwatch/index.js
@@ -1,3 +1,117 @@
 function openWin_clock(){
     window.open("/index.html","_self");
 }
+
+const stp_clock_doc = document.getElementById("stp_clock");
+
+const play_btn_doc = document.getElementById("play_btn");
+const pause_btn_doc = document.getElementById("pause_btn");
+const stop_btn_doc = document.getElementById("stop_btn");
+
+let milli_s = 0;
+let seconds =0;
+let minutes =0;
+
+let beforeZeroMilliSec=0;
+let beforeZeroSec = 0;
+let beforeZeroMin = 0;
+
+let timeInterval = null;
+let time_status = "pause"
+
+
+
+
+
+function startStopWatch(){
+
+    milli_s++;
+    
+    if(milli_s/100===1){
+        milli_s=0;
+        seconds++;
+    }
+    if(seconds/60===1){
+        seconds=0;
+        minutes++;
+    }
+
+
+
+    if(milli_s<10){
+        beforeZeroMilliSec = "0" + milli_s.toString()
+    }
+    else{
+        beforeZeroMilliSec=milli_s.toString()
+    }
+
+    if(seconds<10){
+        beforeZeroSec ="0"+seconds.toString()
+    }
+    else{
+        beforeZeroSec= seconds.toString()
+    }
+
+    if(minutes<10){
+        beforeZeroMin = "0"+minutes.toString()
+    }
+    else{
+        beforeZeroMin=minutes.toString()
+    }
+    
+
+    stp_clock_doc.innerText=`${beforeZeroMin} : ${beforeZeroSec} : ${beforeZeroMilliSec}`;
+
+
+}
+
+play_btn_doc.addEventListener("click",function(){
+    
+    play_btn_doc.classList.add("pressed");
+    pause_btn_doc.classList.remove("pressed");
+    stop_btn_doc.classList.remove("pressed_stp")
+
+
+    if(time_status=="pause"){
+        timeInterval = window.setInterval(startStopWatch,10);
+        time_status="play";
+
+    }
+    
+})
+
+pause_btn_doc.addEventListener("click",function(){
+    play_btn_doc.classList.remove("pressed");
+    pause_btn_doc.classList.add("pressed");
+    stop_btn_doc.classList.remove("pressed_stp")
+
+
+    if(time_status=="play"){
+        window.clearInterval(timeInterval);
+        time_status="pause";
+    }
+})
+
+
+stop_btn_doc.addEventListener("click",function(){
+    pause_btn_doc.classList.remove("pressed");
+    play_btn_doc.classList.remove("pressed");
+
+    stop_btn_doc.classList.add("pressed_stp")
+
+
+    time_status="pause";
+    window.clearInterval(timeInterval);
+    milli_s=0;
+    seconds=0;
+    minutes=0;
+
+    stp_clock_doc.innerText = "00 : 00 : 00";
+})
+
+
+// styling buttons using js
+
+
+
+//window.setInterval(startStopWatch,10)
